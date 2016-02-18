fad297ca5508ac81541fe310b7607b6b6d346b92
Łukasz Wieczorek <lukasz.wieczorek@unit9.com>
2016-02-18 21:29:44 +0100

    Refactored install script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 cd "$DIR"
-cp -r .gitconfig .inputrc .octaverc .terminfo .vimrc.after ~
+cp -r .gitconfig .inputrc .octaverc .terminfo .vimrc.after ~/
+
