0671c90d9a332ff49b44898fdbafc6bfcce5b3d8
Łukasz Wieczorek <lukasz.wieczorek@unit9.com>
2016-02-18 21:30:19 +0100

    Removed terminfo from instal script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "$DIR"
-cp -r .gitconfig .inputrc .octaverc .terminfo .vimrc.after ~/
+cp -r .gitconfig .inputrc .octaverc .vimrc.after ~/
 
