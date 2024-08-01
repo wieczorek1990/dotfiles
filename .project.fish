285c22efac01f426f2b14f23412d3efa71930c54
Łukasz Wieczorek <wieczorek1990@gmail.com>
2024-08-01 15:03:11 +0200

    Modify config.fish to include zsh aliases. Add .project.fish.

---
+++ .project.fish
@@ -0,0 +1,6 @@
+set PROJECT 'Techs/eskulap.synology/'
+set SSH_KEY_POSTFIX '-techs'
+
+set CLUSTER
+set REGION
+set EMAIL
--- config.fish
+++ config.fish
@@ -1,5 +1,15 @@
 set -U fish_greeting
 
+set PATH\
+    /usr/local/opt/node@20/bin\
+    /Applications/Postgres.app/Contents/Versions/16/bin\
+    $PATH
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    source "$HOME/.project.fish"
+
+    alias be="cd ~/Software/$PROJECT"
+    alias init="ssh-add ~/.ssh/id_rsa$SSH_KEY_POSTFIX"
+    alias server="python -m http.server 80"
 end
